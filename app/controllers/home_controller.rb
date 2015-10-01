57e535c65e95dff5aa19816c29adb62ed2496650
insu0616 <insu0616@gmail.com>
2015-10-01 06:28:05 +0000

    rails

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+    def index
+    end
+    
+    def about
+    end
+    
+    def inspiration
+    end
+    
+    def phone
+    end
+    
+    def webprogram
+    end
+    
+end
